de7c3d3d939f057fa74d7a99868ddc9922aa93ca
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2023-04-12 20:43:37 +0300

    Update exist.c

--- exist.c
+++ exist.c
@@ -1,19 +1,74 @@
-#include "header.h"
-/**
- * exist - Verify if a file exist
- * @pathname: path to the file
- * Return: 0 if the file exist or -1 if not
- */
-int exist(char *pathname)
-{
-	int exist_stat;
+#include "shell.h"
 
-	exist_stat = (open(pathname, O_RDONLY));
-	if (exist_stat != -1)
+/**
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
 	{
-		close(exist_stat);
-		return (0);
+		dest[i] = src[i];
+		i++;
 	}
-	else
-		return (-1);
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
